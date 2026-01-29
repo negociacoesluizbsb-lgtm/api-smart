0d305a7472dea69bae6caca9549cd2d48ab6cac3
negociacoesluizbsb-lgtm <negociacoes.luizbsb@gmail.com>
2026-01-29 12:00:54 -0300

    Update models.py

--- models.py
+++ models.py
@@ -17,3 +17,14 @@ class AnaliseCredito:
         self.empresa = empresa
         self.rating = rating
         self.capacidade_endividamento = capacidade_endividamento
+
+class PerfilCredito:
+    def __init__(self, classificacao, justificativa):
+        self.classificacao = classificacao  # Positivo ou Negativo
+        self.justificativa = justificativa
+
+
+class RatingCredito:
+    def __init__(self, nota, justificativa):
+        self.nota = nota  # A, B, C, D
+        self.justificativa = justificativa
